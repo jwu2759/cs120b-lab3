b05f00391c3a39b44c021c59535c2cead569efa1
Min-Chen Wu <mwu134@wch136-22.cs.ucr.edu>
2021-04-11 17:54:21 -0700

    Completed part 5

---
+++ turnin/mw134_lab3_part5.c
@@ -0,0 +1,30 @@
+/*	Author: mwu134
+ *  Partner(s) Name: Min Chen Wu
+ *	Lab Section: 22
+ *	Assignment: Lab 3  Exercise 5
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRD = 0x00; PORTD = 0xFF;
+	DDRB = 0xFE; PORTB = 0x01;
+    /* Insert your solution below */
+	unsigned char tmpA = 0x00;
+	//unsigned char tmpB = 0x00;
+    while (1) {
+	tmpA = (PIND << 1) + (PINB & 0x01);
+	if(tmpA >= 0x46) PORTB = (PORTB & 0xF9) | 0x02;
+	else if(tmpA > 0x05 && tmpA < 0x46) PORTB = (PORTB & 0xF9) | 0x04;
+	else PORTB = (PORTB & 0xF9);
+	PORTB = (PORTB & 0xFE) | (PINB & 0x01);
+    }
+    return 1;
+}
---
+++ turnin/mw134_lab3_part5_tests.py
@@ -0,0 +1,42 @@
+# Array of tests to run (in order)
+# Each test contains
+#   description - 
+#   steps - A list of steps to perform, each step can have
+#       inputs - A list of tuples for the inputs to apply at that step
+#       *time - The time (in ms) to wait before continuing to the next step 
+#           and before checking expected values for this step. The time should be a multiple of
+#           the period of the system
+#       *iterations - The number of clock ticks to wait (periods)
+#       expected - The expected value at the end of this step (after the "time" has elapsed.) 
+#           If this value is incorrect the test will fail early before completing.
+#       * only one of these should be used
+#   expected - The expected output (as a list of tuples) at the end of this test
+# An example set of tests is shown below. It is important to note that these tests are not "unit tests" in 
+# that they are not ran in isolation but in the order shown and the state of the device is not reset or 
+# altered in between executions (unless preconditions are used).
+tests = [ 
+    {'description': 'PIND: 0xFF PINB = 0x01 => PORTB: 0x03',
+    'steps': [ {'inputs': [('PIND',0xFF),('PINB',0x01)], 'iterations': 5 } ],
+    'expected': [('PORTB',0x03)],
+    },
+    {'description': 'PIND: 0x2F PINB = 0x00 => PORTB: 0x04',
+    'steps': [ {'inputs': [('PIND',0x2F),('PINB',0x00)], 'iterations': 5 } ],
+    'expected': [('PORTB',0x02)],
+    },
+    {'description': 'PIND: 0x0F PINB = 0x01 => PORTB: 0x05',
+    'steps': [ {'inputs': [('PIND',0x0F),('PINB',0x01)], 'iterations': 5 } ],
+    'expected': [('PORTB',0x05)],
+    },
+    {'description': 'PIND: 0x00 PINB = 0x01 => PORTB: 0x01',
+    'steps': [ {'inputs': [('PIND',0x00),('PINB',0x01)], 'iterations': 5 } ],
+    'expected': [('PORTB',0x01)],
+    },
+
+
+    ]
+watch = ['tmpA']
+# Optionally you can add a set of "watch" variables these need to be global or static and may need
+# to be scoped at the function level (for static variables) if there are naming conflicts. The 
+# variables listed here will display everytime you hit (and stop at) a breakpoint
+#watch = ['<function>::<static-var>','PORTB']
+
